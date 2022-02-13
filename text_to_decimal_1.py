cef77204f24488adbe70b2c6c70e65c9728abd98
Mohammed Khaldoune <79013612+MedKH1684@users.noreply.github.com>
2022-02-13 08:09:00 -0500

    Delete text_to_decimal_1.py

--- text_to_decimal_1.py
+++
@@ -1,8 +0,0 @@
-msg = "Testing RSA !!"
-
-m = 0
-for i in range (len(msg)):
-	print(msg[i]+"*256**"+str(len(msg)-(i+1))+" + ", end="")
-	m += ord(msg[i])*(256**(len(msg)-(i+1)))
-
-print("\nm=" + str(m))
